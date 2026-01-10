75c8495bf77cf1acc336c7038ddfeccf93e224a1
Ben Nguyen <jl.jimluv@gmail.com>
2026-01-10 15:22:56 +0700

    make MainBtn work with anchors + resume view

--- src/components/buttons/MainBtn.tsx
+++ src/components/buttons/MainBtn.tsx
@@ -1,23 +1,33 @@
-import Link from "next/link";
 import React from "react";
 
 export default function MainBtn({
   classNames = "",
   text,
   icon,
+  href,
+  download = false,
+  target = "",
 }: {
   classNames?: string;
   text: string;
   icon: React.ReactNode;
+  href: string;
+  download?: boolean;
+  target?: string;
 }) {
   return (
-    <Link href={"#"}>
+    <a
+      target={target}
+      href={href}
+      rel="noopener noreferrer"
+      download={download}
+    >
       <button
         className={`${classNames} inline-flex items-center justify-center duration-200 h-14 px-8 text-lg font-bold rounded-full transition-all cursor-pointer gap-2 active:scale-95 disabled:opacity-50`}
       >
         {text}
         {icon}
       </button>
-    </Link>
+    </a>
   );
 }
--- src/components/containers/IntroContainer.tsx
+++ src/components/containers/IntroContainer.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, Download } from "lucide-react";
+import { ArrowRight, Eye } from "lucide-react";
 import MainBtn from "../buttons/MainBtn";
 import Desc from "../typo/Desc";
 import Heading2 from "../typo/Heading2";
@@ -17,13 +17,16 @@ export default function IntroContainer() {
       </Desc>
       <div className="flex justify-center items-center gap-2 py-8">
         <MainBtn
+          href="#contact"
           text="Start a Project"
           icon={<ArrowRight />}
           classNames="shadow-blue-500/25 hover:shadow-blue-500/40 hover:-translate-y-0.5 bg-primary text-black hover:bg-lime-400 shadow-[0_4px_20px_rgba(217,241,84,0.3)] hover:shadow-[0_4px_30px_rgba(217,241,84,0.5)]"
         />
         <MainBtn
-          text="Download Resume"
-          icon={<Download />}
+          target="_blank"
+          href="/Resume_Ben-Nguyen_Full-Stack-Software-Engineer.pdf"
+          text="View Resume"
+          icon={<Eye />}
           classNames="font-semibold border border-2 hover:border-primary border-white/10 hover:bg-white/5 hover:text-amber-100 text-primary backdrop-blur-md"
         />
       </div>
